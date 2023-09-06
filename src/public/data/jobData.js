15547a478ce3d6232e0f793213324de89393eace
ckildare19 <ckildare19@huskers.unl.edu>
2023-09-06 12:10:11 -0500

    Add some job data for Speedway and FYRE

--- src/public/data/jobData.js
+++ src/public/data/jobData.js
@@ -1,68 +1,61 @@
 export const jobData = [
   {
-    jobTitle: "Job title",
+    jobTitle: "Software Engineering Intern",
     companyData: {
-      name: "compnay name",
-      address: "address address 1234 st",
-      link: "www.google.com",
+      name: "Speedway Motors Inc.",
+      address: "340 Victory Ln, Lincoln, NE 68528",
+      link: "https://www.speedwaymotors.com",
       contacts: {
-        email: "me@gmail.com",
-        phone: "jhzsdbc",
+        email: "speedwaymotors@email.speedwaymotors.com",
+        phone: "(800) 979-0122",
       }
     },
     supervisors: [
-      "kjfsk wdsdcsdc",
-      "adam frogger",
-      "mitchell sanders"
+      "Chris Stevenson",
+      "Matt Stubblefield",
     ],
-    startDate: "March 2000",
-    endDate: "May 2001",
+    startDate: "May 2022",
+    endDate: "Current",
     contributions: [
-      "afdh ub b ib bh v u vubjh bkjbii uubibiu iuub.",
-      "iubi bib ib bib inbiu n iun iuniun iubiubiu b.",
-      "j hbb  bijknbk jnij nijnkn kjnkj n jnjknkjn jkjn. hbjbj bijbij n ijni nin niuj.",
-      "jhbjh bjhbkj bkjn kjnkj.",
-      "jhb jhbjhbkj bkjnkjn kjnkjnkjn kn kjn kjnkjn kjn jn kjn kjn."
+      "Increased customer conversions and product impressions by expanding recommendations across the majority of Speedway Motors pages.",
+      "Improved the performance of checkout and shopping cart read / write operations by optimizing related API calls.",
+      "Collaborated with intern cohort to extend the Speedway Motors onboarding process.",
+      "Monitored code deployed into production to ensure no resulting negative customer interactions.",
+      "Aided in development of a Selenium test suite to ensure the validity of code across all environments."
     ],
     outcomes: [
-      "afdh ub b ib bh v u vubjh bkjbii uubibiu iuub.",
-      "iubi bib ib bib inbiu n iun iuniun iubiubiu b.",
-      "j hbb  bijknbk jnij nijnkn kjnkj n jnjknkjn jkjn. hbjbj bijbij n ijni nin niuj.",
-      "jhbjh bjhbkj bkjn kjnkj.",
-      "jhb jhbjhbkj bkjnkjn kjnkjnkjn kn kjn kjnkjn kjn jn kjn kjn."
+      "By developing a microservice for all recommendations from the ground up, I learned how to effectively initialise a microservice.",
+      "I have been able to justify smart decisions around issue creation and work prioritization by providing gathered metrics from Google Analytics.",
+      "I have learned to develop a complex testing suite using several tools such as dotnet, Postman, Selenium, and Playwright to ensure the validity of code.",
+      "I have become more comfortable interviewing customers about their experience with my team's products and gathering feedback on how our work can be improved.",
+      "I have become more experienced designing and executing experiments using feature flags to find the most profitable recommendation configurations."
     ]
   },
   {
-    jobTitle: "Job title",
+    jobTitle: "Web Developer for Yao Labs",
     companyData: {
-      name: "compnay name",
-      address: "address address 1234 st",
-      link: "www.google.com",
+      name: "UNL First Year Research Experience (FYRE) Program,",
+      address: "225 Nebraska Union Lincoln, NE 68588-0451",
+      link: "https://cse.unl.edu/~qyao/",
       contacts: {
-        email: "me@gmail.com",
-        phone: "jhzsdbc",
+        email: "careerservices@unl.edu",
+        phone: "402-472-3145",
       }
     },
     supervisors: [
-      "kjfsk wdsdcsdc",
-      "adam frogger",
-      "mitchell sanders"
+      "Dr. Qiuming Yao"
     ],
-    startDate: "March 2000",
-    endDate: "May 2001",
+    startDate: "August 2021",
+    endDate: "May 2022",
     contributions: [
-      "afdh ub b ib bh v u vubjh bkjbii uubibiu iuub.",
-      "iubi bib ib bib inbiu n iun iuniun iubiubiu b.",
-      "j hbb  bijknbk jnij nijnkn kjnkj n jnjknkjn jkjn. hbjbj bijbij n ijni nin niuj.",
-      "jhbjh bjhbkj bkjn kjnkj.",
-      "jhb jhbjhbkj bkjnkjn kjnkjnkjn kn kjn kjnkjn kjn jn kjn kjn."
+      "Develop web pages using HTML, CSS, Python, JavaScript, and Bootstrap technologies.",
+      "Monitor the growth of the virtual reality industry and coordinate project plans around developing virtual reality simulation software.",
+      "Collaborate with Dr. Yao to learn web design technologies and develop project plans."
     ],
     outcomes: [
-      "afdh ub b ib bh v u vubjh bkjbii uubibiu iuub.",
-      "iubi bib ib bib inbiu n iun iuniun iubiubiu b.",
-      "j hbb  bijknbk jnij nijnkn kjnkj n jnjknkjn jkjn. hbjbj bijbij n ijni nin niuj.",
-      "jhbjh bjhbkj bkjn kjnkj.",
-      "jhb jhbjhbkj bkjnkjn kjnkjnkjn kn kjn kjnkjn kjn jn kjn kjn."
+      "I achieved fundamental knowledge for using HTML, CSS, Python, JavaScript, and Bootstrap technologies.",
+      "I have gotten the opportunity monitoring current events to determine project plans and effecive technologies to implement.",
+      "I have become more comfortable working hands-on with a superior to learn new skills and fortify existing ones."
     ]
   },
   {

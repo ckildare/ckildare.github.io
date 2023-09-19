b8e26480178bf30a0c0fca7c0c117860eca095f6
ckildare19 <ckildare19@huskers.unl.edu>
2023-09-19 11:53:33 -0500

    Add vercel analytics

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './styles/home.module.scss'
 
-import headShot from '../public/my-photos/temp-headshot.png';
+import cornHacks from '../public/my-photos/cornhacks.jpg'
 import photoWithGageFriends from '../public/my-photos/with-gage-friends.jpg';
 
 export default function Home() {
@@ -12,8 +13,8 @@ export default function Home() {
   return (
     <div className={styles.homeContainer}>
       <div className={styles.photoContainer}>
-        <Image width={100} height={100} src={headShot} alt={"A headshot photo of me."}/>
-        <Image width={221} height={218} src={photoWithGageFriends} alt={"Me with my little brother and some friends."}/>
+        <img src={cornHacks} alt={"Picture is of team mates and I during a hackathon event looking tired."}/>
+        <img src={photoWithGageFriends} alt={"Me with my little brother and some friends."}/>
       </div>
       <div className={styles.introductionContainer}>
         <p>{tab}My name is Connor Kildare. I am a Junior in Software Engineering at the University of Nebraska - Lincoln. I am proficient in C#, React, and JavaScript. I am also familiar with C, Python, Jave, etc. </p>

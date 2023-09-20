b5b7b819f9bd89559455da63bced6e7bd2336ec4
ckildare19 <ckildare19@huskers.unl.edu>
2023-09-19 23:58:20 -0500

    Crop photos

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,12 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from './styles/home.module.scss'
 
-import cornHacks from '../public/my-photos/cornhacks.jpg'
+import cornHacks from '../public/my-photos/cornhacks.jpg';
+import speedway from '../public/my-photos/speedway.jpg';
+import jobFair from '../public/my-photos/job-fair-2022.jpg';
+import prom from '../public/my-photos/prom.jpg';
+import wilbur from '../public/my-photos/wilbur.jpg';
+import spriteCranberry from '../public/my-photos/sprite-cranberry.jpg';
 import photoWithGageFriends from '../public/my-photos/with-gage-friends.jpg';
 
 export default function Home() {
@@ -13,8 +18,13 @@ export default function Home() {
   return (
     <div className={styles.homeContainer}>
       <div className={styles.photoContainer}>
-        <img src={cornHacks} alt={"Picture is of team mates and I during a hackathon event looking tired."}/>
-        <img src={photoWithGageFriends} alt={"Me with my little brother and some friends."}/>
+        <Image width={1000} height={450} src={cornHacks} alt={"Picture is of team mates and I during a hackathon event looking tired."}/>
+        <Image src={speedway} alt={"Me with my little brother and some friends."}/>
+        <Image src={jobFair} alt={"Me with my little brother and some friends."}/>
+        <Image width={900} height={700} src={prom} alt={"Me with my little brother and some friends."}/>
+        <Image src={wilbur} alt={"Me with my little brother and some friends."}/>
+        <Image src={spriteCranberry} alt={"Me with my little brother and some friends."}/>
+        <Image width={500} height={500} src={photoWithGageFriends} alt={"Me with my little brother and some friends."}/>
       </div>
       <div className={styles.introductionContainer}>
         <p>{tab}My name is Connor Kildare. I am a Junior in Software Engineering at the University of Nebraska - Lincoln. I am proficient in C#, React, and JavaScript. I am also familiar with C, Python, Jave, etc. </p>

7814ccd9fa72771f900b05f2683da19f80cba877
Connor KilDARE <ckildare10@gmail.com>
2023-09-29 15:52:00 -0500

    Better home styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import CardWrapper from '@/components/cards/cardWrapper/cardWrapper';
 import styles from './styles/home.module.scss'
 
 import speedway from '../public/my-photos/speedway.jpg';
@@ -34,9 +35,9 @@ export default function Home() {
         <Image width={isMobile ? 350 : 500} height={isMobile ? 210 : 300} src={prom} alt={"My friends and I at prom."} />
       </div>
       <div className={styles.introductionContainer}>
-        <p>{tab}My name is Connor Kildare. I am a Junior in Software Engineering at the University of Nebraska - Lincoln. I am proficient in C#, React, and JavaScript. I am also familiar with C, Python, Jave, etc. </p>
-        <p>{tab}I aspire to be a Software Engineer to produce accessible code with a meaningful impact to my users. I also hope to gain diverse work experience working in Software. </p>
-        <p>{tab}In my free time, I work on projects that can help me or those around me in day-to-day life. I work with some friends under the team name, “Buddy Systems”, where we periodically meet to work on projects that interest us. I formerly was a Vice-President of the Operating Systems and Open-Source Group at UNL, and now participate in UNL&apos;s Game Dev club.</p>
+        <CardWrapper>{tab}My name is Connor Kildare. I am a Junior in Software Engineering at the University of Nebraska - Lincoln. I am proficient in C#, React, and JavaScript. I am also familiar with C, Python, Jave, etc. </CardWrapper>
+        <CardWrapper>{tab}I aspire to be a Software Engineer to produce accessible code with a meaningful impact to my users. I also hope to gain diverse work experience working in Software. </CardWrapper>
+        <CardWrapper>{tab}In my free time, I work on projects that can help me or those around me in day-to-day life. I work with some friends under the team name, “Buddy Systems”, where we periodically meet to work on projects that interest us. I formerly was a Vice-President of the Operating Systems and Open-Source Group at UNL, and now participate in UNL&apos;s Game Dev club.</CardWrapper>
         <div className={styles.bottomPhotoContainer}>
           <Image width={isMobile ? 200 : 300} height={isMobile ? 200 : 300} src={jobFair} alt={"Me at the 2023 UNL Fall Career Fair."} />
           <Image width={isMobile ? 200 : 300} height={isMobile ? 200 : 300} src={jaxAndMe} alt={"My little brother, Jax, and I."} />

847c3053fd6535b20aae6b8d5b78708631edcce6
Connor KilDARE <ckildare10@gmail.com>
2023-12-09 13:30:33 -0600

    Some cleanup this morning

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -1,14 +1,12 @@
 import styles from './header.module.scss';
-import headshot from '../../../public/my-photos/temp-headshot.png';
-import Link from 'next/link';
-import Image from 'next/image';
 
 const Header = () => {
+
   return (
     <header className={styles.header}>
       <div className={styles.name}>Connor Kildare</div>
       <div className={styles.profession}>Software Developer</div>
-      <div className={styles.studies}>Software Engineering major at the University of Nebraska - Lincoln</div>
+      <div className={styles.studies}>Software Engineering Major at the University of Nebraska - Lincoln</div>
       <div className={styles.contactInfo}>
         <div className={styles.placeholderContact}/>
         <div className={styles.placeholderContact}/>
@@ -16,6 +14,12 @@ const Header = () => {
         <div className={styles.placeholderContact}/>
         <div className={styles.placeholderContact}/>
       </div>
+      {/* <div className={styles.navBar}>
+        <div className={styles.navItem} onClick={() => onNavClick(aboutRef)} >About</div>
+        <div className={styles.navItem} onClick={() => workElement && workElement.scrollIntoView({ behavior: "smooth" })}>Work</div>
+        <div className={styles.navItem} onClick={() => onNavClick(projectsRef)}>Projects</div>
+        <div className={styles.navItem} onClick={() => onNavClick(schoolRef)}>School</div>
+      </div> */}
     </header>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Index() {
         <title>Connor Kildare</title>
         <meta name="description" content="Portfolio site for Connor Kildare" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="keywords" content="Connor Kildare, Connor, Kildare, UNL, Conner Kildare, Conner, Portfolio, Portfolio Site" />
+        <meta name="keywords" content="Connor Kildare, Connor, Kildare, UNL, Conner Kildare, Conner, Portfolio, Portfolio Site, Speedway Motors, Senior Design" />
         <meta name="author" content="Connor Kildare" />
         <link rel="icon" href="/favicon.ico" />
       </Head>

d4e1d17e41f485f80243a4fe6f798b89d8a3f23b
Connor KilDARE <ckildare10@gmail.com>
2023-12-20 00:07:37 -0600

    Add navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,94 @@
 import Head from 'next/head';
-import Header from '@/components/layout/header/header';
+import Link from 'next/link';
+import Image from 'next/image';
 import styles from '@/styles/index.module.scss';
 import About from '@/components/layout/about';
 import Work from '@/components/layout/work';
 import Projects from '@/components/layout/projects';
 import { useRef, useEffect, useState, use } from 'react';
-
-const Section = {
-  About: 0,
-  Work: 1,
-  Projects: 2
-}
+import classNames from 'classnames';
+import githubLogo from '@/public/icons/github.svg';
+import gitlabLogo from '@/public/icons/gitlab.svg';
+import linkedinLogo from '@/public/icons/linkedin.svg';
+import emailLogo from '@/public/icons/email.svg';
+import { resume } from './resume.js';
 
 export default function Index() {
+  const headerRef = useRef(null);
   const aboutRef = useRef(null);
   const workRef = useRef(null);
   const projectsRef = useRef(null);
 
-  const [intersectingSection, setIntersectingSection] = useState(Section.About);
+  const [intersectingSection, setIntersectingSection] = useState(0);
+  const [isHeaderIntersecting, setIsHeaderIntersecting] = useState(true);
+
+  const openPdf = () => {
+    const byteCharacters = window?.atob(resume);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: "application/pdf" });
+    const blobUrl = URL.createObjectURL(blob);
+    window.open(blobUrl);
+  }
 
   useEffect(() => {
-    const observer = new IntersectionObserver(([entry]) => {
-      setIntersectingSection(Section.About);
+    console.log("isHeaderIntersecting: ", isHeaderIntersecting);
+  }, [isHeaderIntersecting]);
+
+  useEffect(() => {
+    const headerObserver = new IntersectionObserver(([entry]) => {
+      setIsHeaderIntersecting(entry.isIntersecting);
     },
-      { rootMargin: "-300px" });
-    observer.observe(aboutRef.current);
-    return () => observer.disconnect();
-  }, [aboutRef]);
+      {
+        rootMargin: "5px"
+      });
+    headerObserver.observe(headerRef.current);
+    return () => headerObserver.disconnect();
+  }, [headerRef, intersectingSection]);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(([entry]) => {
-      setIntersectingSection(Section.Work);
+    const aboutObserver = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) setIntersectingSection(0);
     },
-      { rootMargin: "-300px" });
-    observer.observe(workRef.current);
-    return () => observer.disconnect();
-  }, [workRef]);
+      {
+        rootMargin: "-30% 0px -70% 0px"
+      });
+    aboutObserver.observe(aboutRef.current);
+    return () => aboutObserver.disconnect();
+  }, [aboutRef, intersectingSection]);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(([entry]) => {
-      if (entry.isIntersecting) setIntersectingSection(Section.Projects);
+    const workObserver = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) setIntersectingSection(1);
     },
-      { rootMargin: "-300px" });
-    observer.observe(projectsRef.current);
-    return () => observer.disconnect();
-  }, [projectsRef]);
+      {
+        rootMargin: "-30% 0px -70% 0px"
+      });
+    workObserver.observe(workRef.current);
+    return () => workObserver.disconnect();
+  }, [workRef, intersectingSection]);
 
   useEffect(() => {
-    console.log(`Intersecting section: ${intersectingSection}`);
+    const projectsObserver = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) setIntersectingSection(2);
+    },
+      {
+        rootMargin: "-30% 0px -70% 0px"
+      });
+    projectsObserver.observe(projectsRef.current);
+    console.log('observe');
+    return () => projectsObserver.disconnect();
+  }, [projectsRef, intersectingSection]);
+
+  useEffect(() => {
+    console.log(intersectingSection);
   }, [intersectingSection]);
+
+  const stickyClass = isHeaderIntersecting ? styles.none : styles.sticky;
+  const activeClass = (i) => intersectingSection == i ? styles.active : styles.none;
 
   return (
     <>
@@ -62,19 +102,41 @@ export default function Index() {
       </Head>
 
       <main className={styles.main}>
-        <Header />
+        <header className={styles.header}>
+          <div className={styles.headerContent} ref={headerRef}>
+            <div className={styles.name}>Connor Kildare</div>
+            <div className={styles.profession}>Software Developer</div>
+            <div className={styles.studies}>Software Engineering Major at the University of Nebraska - Lincoln</div>
+            <div className={styles.links}>
+              <Link href={"https://github.com/ckildare"} >
+                <Image className={styles.linkSvg} src={githubLogo} alt={"Github link"} />
+              </Link>
+              <Link href={"https://gitlab.com/ckildare19"}>
+                <Image className={styles.linkSvg} src={gitlabLogo} alt={"Gitlab link"} />
+              </Link>
+              <Link href={"https://linkedin.com/in/connor-kildare"}>
+                <Image className={styles.linkSvg} src={linkedinLogo} alt={"Linkedin link"} />
+              </Link>
+              <Link href={"mailto:ckildare10@gmail.com"}>
+                <Image className={styles.linkSvg} src={emailLogo} alt={"Email link"} />
+              </Link>
+            </div>
+          </div>
+          <div className={classNames(styles.navBar, stickyClass)}>
+            <div className={classNames(styles.navItem, activeClass(0))} onClick={() => aboutRef.current.scrollIntoView({ behavior: "smooth", block: "start" })} >About</div>
+            <div className={classNames(styles.navItem, activeClass(1))} onClick={() => workRef.current.scrollIntoView({ behavior: "smooth", block: "start" })}>Work</div>
+            <div className={classNames(styles.navItem, activeClass(2))} onClick={() => projectsRef.current.scrollIntoView({ behavior: "smooth", block: "start" })}>Projects</div>
+            <div className={styles.navItem} onClick={() => openPdf()}>Resume</div>
+          </div>
+        </header>
         <div className={styles.pageContent}>
-          <section>
-            <div ref={aboutRef} className={styles.sectionHeading}>About Me</div>
+          <section ref={aboutRef}>
             <About />
           </section>
-          <section>
-            <div ref={workRef} className={styles.sectionHeading}>Work Experience</div>
+          <section ref={workRef}>
             <Work />
           </section>
-          <section>
-            <div ref={projectsRef} />
-            <div className={styles.sectionHeading}>Projects</div>
+          <section ref={projectsRef}>
             <Projects />
           </section>
         </div>
---
+++ src/public/enums/section.js
@@ -0,0 +1,5 @@
+export const Section = {
+  About: 0,
+  Work: 1,
+  Projects: 2
+}
